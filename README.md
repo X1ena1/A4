9f44d9fd1f96c934e6f74e2d43214055520511be
X1ena1 <gianellamisslet@gmail.com>
2024-12-03 00:06:15 -1000

    project start

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # A4
+# Hello! I will be creating a reccomendation system baed your preferences and present to you films you could be interewted in 
---
+++ WebSite/Web.py
@@ -0,0 +1,12 @@
+#Importtting files
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return("Welcome CineMatch")
+
+#Run the application
+if __name__ == "__main__":
+    app.run(debug=True)
