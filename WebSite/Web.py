75bfb0c048ff98d76d7cc1b466e851784dccab02
X1ena1 <gianellamisslet@gmail.com>
2024-12-04 02:29:09 -1000

    Routes added along with edits

--- WebSite/Web.py
+++ WebSite/Web.py
@@ -1,12 +1,28 @@
 #Importtting files
 from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
+
+#reading the CSV file
+movies_df = pd.read_csv('IMDB-Movie-Data.csv')
 
 app = Flask(__name__)
 
+#Home route
 @app.route("/")
 def index():
-    return("Welcome CineMatch")
+    return render_template('index.html')
 
-#Run the application
+#Result route
+@app.route("/results")
+def results():
+
+    #Reads top movies
+    movies = movies_df.head()
+
+    print(movies.head())
+
+    return render_template('results.html', movies=movies)
+
+#Running application
 if __name__ == "__main__":
     app.run(debug=True)
